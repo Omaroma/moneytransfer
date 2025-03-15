ca503fc47747a200dbd340e286c33208f0f13144
Omar_Abd_El_Mageed <eng.omar9610@gmail.com>
2025-03-15 22:35:36 +0200

    complete trasnfer and delete user functionality

--- apis/apis.go
+++ apis/apis.go
@@ -11,6 +11,7 @@ func AddUser(c *gin.Context) {
 	userName := c.Query("name")
 	if userName == "" {
 		c.String(http.StatusBadRequest, "you must set user name")
+		return
 	}
 
 	userBalance, _ := strconv.ParseFloat(c.Query("balance"), 64)
@@ -24,11 +25,42 @@ func AddUser(c *gin.Context) {
 }
 
 func DeleteUser(c *gin.Context) {
-	c.String(200, "user deleted successfully")
+	userId := c.Query("id")
+	if userId == "" {
+		c.String(http.StatusBadRequest, "you must enter user id")
+		return
+	}
+
+	services.DeleteUser(userId)
+	c.String(http.StatusOK, "user deleted successfully")
 }
 
 func Transfer(c *gin.Context) {
-	c.String(200, "amount added successfully")
+	senderId := c.Query("from")
+	if senderId == "" {
+		c.String(http.StatusBadRequest, "you must set sender Id")
+		return
+	}
+
+	receiverId := c.Query("to")
+	if receiverId == "" {
+		c.String(http.StatusBadRequest, "you must set receiver Id")
+		return
+	}
+
+	amount, _ := strconv.ParseFloat(c.Query("amount"), 64)
+	if amount == 0 {
+		c.String(http.StatusBadRequest, "please enter transfer amount")
+		return
+	}
+
+	err := services.TransferMoney(senderId, receiverId, amount)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, "amount added successfully")
 }
 
 func GetBalance(c *gin.Context) {
