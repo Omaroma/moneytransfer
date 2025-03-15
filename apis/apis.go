c30b084385e9ffb4b6480b1ae2a505789942c25c
Omar_Abd_El_Mageed <eng.omar9610@gmail.com>
2025-03-15 22:22:32 +0200

    complete add user functionality

--- apis/apis.go
+++ apis/apis.go
@@ -1,9 +1,26 @@
 package apis
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Omaroma/moneytransfer/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func AddUser(c *gin.Context) {
-	c.String(200, "user added successfully")
+	userName := c.Query("name")
+	if userName == "" {
+		c.String(http.StatusBadRequest, "you must set user name")
+	}
+
+	userBalance, _ := strconv.ParseFloat(c.Query("balance"), 64)
+	if userBalance < 0 {
+		c.String(http.StatusBadRequest, "balance can't be negative")
+		return
+	}
+
+	services.AddUser(userName, userBalance)
+	c.String(http.StatusCreated, "user added successfully")
 }
 
 func DeleteUser(c *gin.Context) {
