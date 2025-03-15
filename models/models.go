ab7a369eb0d8c1043d35c2b1e37d4c048bef56f8
Omar_Abd_El_Mageed <eng.omar9610@gmail.com>
2025-03-15 21:20:06 +0200

    add system user model and users store

---
+++ models/models.go
@@ -0,0 +1,40 @@
+package models
+
+import "sync"
+
+type User struct {
+	Id      string
+	Name    string
+	Balance float64
+}
+
+type SystemUsers struct {
+	mu        sync.Mutex
+	userStore map[string]User
+}
+
+func (s *SystemUsers) Add(user User) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.userStore[user.Id] = user
+}
+
+func (s *SystemUsers) Get(userId string) User {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.userStore[userId]
+}
+
+func (s *SystemUsers) Delete(userId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.userStore, userId)
+}
+
+func (s *SystemUsers) ChangeBalance(userId string, newBalance float64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	user := s.userStore[userId]
+	user.Balance = newBalance
+	s.userStore[userId] = user
+}
---
+++ variables/users.go
@@ -0,0 +1,5 @@
+package variables
+
+import "github.com/Omaroma/moneytransfer/models"
+
+var Users models.SystemUsers
